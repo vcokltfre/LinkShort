10e32f487acf1a15928d0fa527653eac4fcc5f42
vcokltfre <vcokltfre@gmail.com>
2020-12-26 23:31:31 +0000

    fix: reconnect to db if initial connect fails

--- src/database.py
+++ src/database.py
@@ -25,6 +25,8 @@ class DatabaseAPI:
         self.idg = IDGenerator()
 
     async def ensure_init(self):
+        if not hasattr(self, "pool"):
+            self.has_init = False
         if not self.has_init:
             self.has_init = True
             await self.init()
